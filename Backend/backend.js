7e40539f843ed9d0e42429013ebc205be031911b
Mayadihny <mayadihny@gmail.com>
2019-06-26 16:31:29 +0300

    Create

--- Backend/backend.js
+++ Backend/backend.js
@@ -19,11 +19,32 @@ app.get('/products/shoes/men', (req,res)=>{
 db.serialize(() => {
   db.all(`SELECT * FROM Products WHERE Type='M' AND Category='S'`, (err, row) => {
     if (err) {
-      res.send({messege:"err.message"})
+      res.send({message:err.message})
     }
     res.send({data:row})
   });
 });
 });
 
- 
+app.get('/Products/create', (req, res) =>{
+  var dataCategory = req.query.Category;
+  var dataType = req.query.Type;
+  var dataPicture = req.query.Picture;
+  var dataTitle = req.query.Title;
+  var dataDescription = req.query.Description;
+  var dataPrice = req.query.Price;
+  var dataSize = req.query.Size;
+
+  db.serialize(() => {
+    db.all(`INSERT INTO Products 
+    (Category, Type, Picture, Title, Description, Price, Size)
+    VALUES (? , ?, ?, ?, ?, ?, ?)
+    `,[dataCategory, dataType, dataPicture, dataTitle, dataDescription, dataPrice, dataSize ], (err, row) => {
+      if (err) {
+        res.send({message:err.message})
+      }
+      res.send({data:row})
+    });
+  });
+  });
+
